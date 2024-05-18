68671995fa70f0aec8744db5384f1c7df966b753
ComputerGuy <vicmee76@hotmail.com>
2024-05-18 17:51:56 +0100

    -- implemented service for creating a new user

---
+++ Components/Data/Services/Accounts/UserService.cs
@@ -0,0 +1,29 @@
+﻿using ivs_ui.Domain.Constants;
+using ivs_ui.Domain.Interfaces.Accounts;
+using ivs_ui.Domain.Interfaces.General;
+using ivs_ui.Domain.Models.Dtos.Organisations;
+using ivs_ui.Domain.Models.ViewModels.Accounts;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace ivs_ui.Components.Data.Services.Accounts
+{
+    public class UserService(IWebService webService) : IUserService
+    {
+        private readonly IWebService _webService = webService;
+        private readonly string apiUrl = "/api/v1/users";
+
+        public async Task<ResponseObject> CreateUser(SignUpVM signUpVM)
+        {
+            var response = await _webService.Call(apiUrl, "/sign-up/create-user", Method.Post, signUpVM);
+            var res = JsonConvert.DeserializeObject<ResponseObject>(response.Content ?? "");
+            var content = res.result;
+            if (content?.code != ResponseCodes.ResponseCode_Created)
+                return res;
+
+            var myJsonResponse = content?.data.ToString().Trim().TrimStart('{').TrimEnd('}');
+            res.result.data = JsonConvert.DeserializeObject<List<string>>(myJsonResponse);
+            return res;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 using ivs_ui.Components;
 using ivs_ui.Components.Data.Helpers;
+using ivs_ui.Components.Data.Services.Accounts;
 using ivs_ui.Components.Data.Services.General;
 using ivs_ui.Components.Data.Services.Organisations;
+using ivs_ui.Domain.Interfaces.Accounts;
 using ivs_ui.Domain.Interfaces.General;
 using ivs_ui.Domain.Interfaces.Organisations;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,10 +15,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopRight;
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 4000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
 
 builder.Services.AddScoped<IWebService, WebService>();
 builder.Services.AddTransient<IOrganisationService, OrganisationService>();
+builder.Services.AddTransient<IUserService, UserService>();
 
 var app = builder.Build();
 
