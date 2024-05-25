4a463aef8c04db1073364831fdd684db4adfef13
ComputerGuy <vicmee76@hotmail.com>
2024-05-25 16:02:32 +0100

    -- added check for logged in user on login page or sign up page

--- Components/Data/Services/Organisations/OrganisationService.cs
+++ Components/Data/Services/Organisations/OrganisationService.cs
@@ -14,15 +14,28 @@ namespace ivs_ui.Components.Data.Services.Organisations
 
         public async Task<ResponseObject> GetOrganisations()
         {
-            var response = await _webService.Call(apiUrl, "", Method.Get, null, null);
-            var res = JsonConvert.DeserializeObject<ResponseObject>(response.Content ?? "");
-            var content = res.result;
-            if (content?.code != ResponseCodes.ResponseCode_Successful)
-                return new ResponseObject();
+            try
+            {
+                var response = await _webService.Call(apiUrl, "", Method.Get, null, null);
+                var res = JsonConvert.DeserializeObject<ResponseObject>(response.Content ?? "");
+                var content = res.result;
+                if (content?.code != ResponseCodes.ResponseCode_Successful)
+                    return new ResponseObject();
 
-            var myJsonResponse = content?.data.ToString().Trim().TrimStart('{').TrimEnd('}');
-            res.result.data = JsonConvert.DeserializeObject<List<GetOrganisationsDto>>(myJsonResponse);
-            return res;
+                var myJsonResponse = content?.data.ToString().Trim().TrimStart('{').TrimEnd('}');
+                res.result.data = JsonConvert.DeserializeObject<List<GetOrganisationsDto>>(myJsonResponse);
+                return res;
+            }
+            catch (Exception ex)
+            {
+                return new ResponseObject()
+                {
+                    result = new ResponseContents()
+                    {
+                        message = "Error! Something went wrong, please try agian later",
+                    }
+                };
+            }
         }
     }
 }
