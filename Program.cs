72bec97d5123ee880532fb719b0450fcd86fedcf
ComputerGuy <vicmee76@hotmail.com>
2024-06-04 16:25:08 +0100

    -- implemented get all payment options

--- Program.cs
+++ Program.cs
@@ -1,20 +1,16 @@
 using Blazored.SessionStorage;
 using ivs_ui.Components;
-using ivs_ui.Components.Data.Helpers;
 using ivs_ui.Components.Data.Services.Accounts;
 using ivs_ui.Components.Data.Services.Events;
 using ivs_ui.Components.Data.Services.General;
 using ivs_ui.Components.Data.Services.Organisations;
+using ivs_ui.Components.Data.Services.Payment;
 using ivs_ui.Domain.Interfaces.Accounts;
 using ivs_ui.Domain.Interfaces.Events;
 using ivs_ui.Domain.Interfaces.General;
 using ivs_ui.Domain.Interfaces.Organisations;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using ivs_ui.Domain.Interfaces.Payment;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using MudBlazor;
 using MudBlazor.Services;
 
@@ -49,6 +45,7 @@ builder.Services.AddScoped<IWebService, WebService>();
 builder.Services.AddTransient<IOrganisationService, OrganisationService>();
 builder.Services.AddTransient<IAccountService, AccountService>();
 builder.Services.AddTransient<IEventTypeService, EventTypeService>();
+builder.Services.AddTransient<IPaymentOptionService, PaymentOptionService>();
 
 
 
