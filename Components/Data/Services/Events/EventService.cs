bd820fb9f39d44d9471d4665d54f14e4f04df3e4
ComputerGuy <vicmee76@hotmail.com>
2024-06-05 22:02:28 +0100

    - implemented create event service

--- Components/Data/Services/Events/EventService.cs
+++ Components/Data/Services/Events/EventService.cs
@@ -3,36 +3,31 @@ using ivs_ui.Components.Data.Services.General;
 using ivs_ui.Domain.Constants;
 using ivs_ui.Domain.Interfaces.Events;
 using ivs_ui.Domain.Interfaces.General;
-using ivs_ui.Domain.Models.Dtos.Accounts;
-using ivs_ui.Domain.Models.Dtos.Events.EventTypes;
+using ivs_ui.Domain.Models.ViewModels.Events;
 using Newtonsoft.Json;
 using RestSharp;
 using System.Reflection;
 
 namespace ivs_ui.Components.Data.Services.Events
 {
-    public class EventTypeService(IWebService webService, ISessionStorageService sessionStorageService) : IEventTypeService
+    public class EventService(IWebService webService, ISessionStorageService sessionStorageService) : IEventService
     {
         private readonly IWebService _webService = webService;
         private readonly ISessionStorageService _sessionStorageService = sessionStorageService;
-        private readonly string apiUrl = "/api/v1/event-types";
+        private readonly string apiUrl = "/api/v1/ivs-event/";
 
-
-        public async Task<ResponseObject> GetEventTypes()
+        public async Task<ResponseObject> CreateEvent(CreateEventVM model)
         {
             try
             {
                 var token = await _sessionStorageService.GetItemAsync<string>(Tokens.TokenName);
                 var headers = new Dictionary<string, string> { { "Authorization", $"Bearer {token}" } };
 
-                var response = await _webService.Call(apiUrl, "/", Method.Get, null, headers);
+                var response = await _webService.Call(apiUrl, "create-event", Method.Post, model, headers);
                 var res = JsonConvert.DeserializeObject<ResponseObject>(response.Content ?? "");
                 var content = res.result;
-                if (content?.code != ResponseCodes.ResponseCode_Successful)
+                if (content?.code != ResponseCodes.ResponseCode_Created)
                     return res;
-
-                var myJsonResponse = content.data.ToString().Trim().TrimStart('{').TrimEnd('}');
-                res.result.data = JsonConvert.DeserializeObject<List<GetEventTypesDto>>(content.data.ToString());
                 return res;
             }
             catch (Exception ex)
