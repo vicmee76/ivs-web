16a0ff7e726b264e6f6d0bfb8024294e5742f0f1
ComputerGuy <vicmee76@hotmail.com>
2024-06-20 16:11:49 +0100

    -- added new loading animation for event name and new design for activate event

--- Components/Data/Services/Events/EventService.cs
+++ Components/Data/Services/Events/EventService.cs
@@ -76,7 +76,34 @@ namespace ivs_ui.Components.Data.Services.Events
             }
         }
 
+        public async Task<ResponseObject> GetEventDetails(string id)
+        {
+            try
+            {
+                var token = await _sessionStorageService.GetItemAsync<string>(Tokens.TokenName);
+                var headers = new Dictionary<string, string> { { "Authorization", $"Bearer {token}" } };
 
+                var response = await _webService.Call(apiUrl, $"get-ivs-event-by-id/{id}", Method.Get, null, headers);
+                var res = JsonConvert.DeserializeObject<ResponseObject>(response.Content ?? "");
+                var content = res.result;
+                if (content?.code != ResponseCodes.ResponseCode_Ok)
+                    return res;
+
+                var myJsonResponse = content.data.ToString().Trim().TrimStart('{').TrimEnd('}');
+                res.result.data = JsonConvert.DeserializeObject<GetEventDetailsDto>(content.data.ToString());
+                return res;
+            }
+            catch (Exception ex)
+            {
+                return new ResponseObject()
+                {
+                    result = new ResponseContents()
+                    {
+                        message = "Error! Something went wrong trying to get event meta data, please try agian later",
+                    }
+                };
+            }
+        }
 
         public async Task<ResponseObject> GetEventMetaData(string id)
         {
