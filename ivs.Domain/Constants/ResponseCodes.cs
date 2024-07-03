414292985417acbe42bbf4f4fb1713c6b7965818
Victor Nwogu <vicmee76@hotmail.com>
2024-07-03 17:00:48 +0100

    -- app changes

--- ivs.Domain/Constants/ResponseCodes.cs
+++ ivs.Domain/Constants/ResponseCodes.cs
@@ -3,5 +3,5 @@ namespace ivs.Domain.Constants;
 public static class ResponseCodes
 {
     public static readonly int ResponseCodeCreated = 201;
-    public static readonly int ResponseCodeOk = 201;
+    public static readonly int ResponseCodeOk = 200;
 }
