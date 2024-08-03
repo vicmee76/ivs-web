42732650416cbdc8afba48a295a1ff9c38f40842
ComputerGuy <vicmee76@hotmail.com>
2024-08-03 06:25:25 +0100

    -- added new page for the discover different event options (free, paid and nothing)

--- ivs.Domain/Constants/Helpers.cs
+++ ivs.Domain/Constants/Helpers.cs
@@ -5,6 +5,8 @@ public class Helpers
     public static readonly string Active = "ACTIVE";
     public static readonly string Pending = "PENDING";
     public static readonly string GetOptionsLatest = "Latest";
+    public static readonly string FreeEvents = "FREE";
+    public static readonly string PaidEvents = "PAID";
     public static readonly string DiscoverEventLatest = "latest-event";
     public static readonly string DiscoverEventFree = "free-event";
     public static readonly string DiscoverEventPaid = "paid-event";
