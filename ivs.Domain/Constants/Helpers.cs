8023a78d5215b1fcc440e2fb60d0a738ad5154b7
ComputerGuy <vicmee76@hotmail.com>
2024-08-03 05:29:19 +0100

    -- implemented feature to see latest events

--- ivs.Domain/Constants/Helpers.cs
+++ ivs.Domain/Constants/Helpers.cs
@@ -4,4 +4,5 @@ public class Helpers
 {
     public static readonly string Active = "ACTIVE";
     public static readonly string Pending = "PENDING";
+    public static readonly string GetOptionsLatest = "Latest";
 }
