0dc72128a5aecd9b39215789f9c6429303ba569f
ComputerGuy <vicmee76@hotmail.com>
2024-08-10 07:29:58 +0100

    -- added new banner slider for events and change layouts

--- Components/Data/Services/Events/EventTypeService.cs
+++ Components/Data/Services/Events/EventTypeService.cs
@@ -20,10 +20,7 @@ namespace ivs_ui.Components.Data.Services.Events
         {
             try
             {
-                var token = await _sessionStorageService.GetItemAsync<string>(Tokens.TokenName);
-                var headers = new Dictionary<string, string> { { "Authorization", $"Bearer {token}" } };
-
-                var response = await _webService.Call(ApiUrl, "/", Method.Get, null, headers);
+                var response = await _webService.Call(ApiUrl, "/", Method.Get, null, null);
                 var res = JsonConvert.DeserializeObject<ResponseObject>(response.Content ?? "");
                 var content = res?.result;
                 if (content?.code != ResponseCodes.ResponseCodeOk)
