8470fda9e3873ae188854e1f7f2edce7ac592389
ComputerGuy <vicmee76@hotmail.com>
2024-08-24 04:28:45 +0100

    -- added contact information VM

---
+++ ivs.Domain/Models/ViewModels/Orders/ContactInformationVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ivs.Domain.Models.ViewModels.Orders
+{
+    public class ContactInformationVM
+    {
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        public string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        public string? Gender { get; set; }
+
+    }
+}
