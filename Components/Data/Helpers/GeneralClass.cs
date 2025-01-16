916c8e2adc24afa9562d766ba3da9c0f455d2aab
Victor Nwogu <vicmee76@hotmail.com>
2025-01-16 14:33:11 +0100

    -- added user details page and made update to hove account bumbers are viewed

--- Components/Data/Helpers/GeneralClass.cs
+++ Components/Data/Helpers/GeneralClass.cs
@@ -11,5 +11,16 @@ namespace ivs_ui.Components.Data.Helpers
             TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
             return textInfo.ToTitleCase(input.ToLower());
         }
+        
+        public static string MaskNumber(string number)
+        {
+            if (number.Length <= 4)
+                return number; // If the number is too short, just return it as-is.
+
+            var lengthToMask = number.Length - 4;
+            var maskedPart = new string('*', lengthToMask);
+            var visiblePart = number.Substring(lengthToMask);
+            return maskedPart + visiblePart;
+        }
     }
 }
