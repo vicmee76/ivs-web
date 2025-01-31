ae6a3c79901ab2756d088ba5a562d255da4867be
ComputerGuy <vicmee76@hotmail.com>
2025-01-31 02:23:23 +0100

    -- implemented views for user settlement and user sales

--- ivs.Domain/Interfaces/Payment/IPaymentService.cs
+++ ivs.Domain/Interfaces/Payment/IPaymentService.cs
@@ -10,6 +10,7 @@ namespace ivs.Domain.Interfaces.Payment
         public Task<ResponseObject> VerifyPayment(Dictionary<string, string> model);
         public Task<ResponseObject> ProcessFreePayment(string orderId);
         public Task<ResponseObject> GetSales(Dictionary<string, string> queryParam);
+        public Task<ResponseObject> GetUserSales(Dictionary<string, string> queryParam);
         public Task<ResponseObject> GetBanks(string country = "NG");
         public Task<ResponseObject> VerifyAccountNumber(string bankCode, string accountNumber);
         public Task<ResponseObject> GetTransferFee(decimal settlementAmount, string eventId);
