575fbd50ba956a39b5b3b59d23ad9da0356d4e8e
ComputerGuy <vicmee76@hotmail.com>
2025-02-01 11:23:35 +0100

    -- added services for event types

--- Components/Data/Services/Events/EventTypeService.cs
+++ Components/Data/Services/Events/EventTypeService.cs
@@ -6,13 +6,15 @@ using ivs.Domain.Constants;
 using ivs.Domain.Interfaces.Events;
 using ivs.Domain.Interfaces.General;
 using ivs.Domain.Models.Dtos.Events;
+using ivs.Domain.Models.Dtos.Organisations;
+using System.Reflection;
 
 namespace ivs_ui.Components.Data.Services.Events
 {
     public class EventTypeService(IWebService webService) : IEventTypeService
     {
         private readonly IWebService _webService = webService;
-        private const string ApiUrl = "/api/v1/event-types";
+        private const string ApiUrl = "/api/v1/event-types/";
 
 
         public async Task<ResponseObject> GetEventTypes()
@@ -35,6 +37,108 @@ namespace ivs_ui.Components.Data.Services.Events
                     result = new ResponseContents()
                     {
                         message = "Error! Something went wrong trying to get all event types, please try again later",
+                    }
+                };
+            }
+        }
+
+
+            
+        public async Task<ResponseObject> CreateEventTypes(CreateEentTypesDto model)
+        {
+            try
+            {
+                var headers = await _webService.GetAuthorizationHeaders();
+                var response = await _webService.Call(ApiUrl, $"create-event-type", Method.Post, model, headers);
+                var res = JsonConvert.DeserializeObject<ResponseObject>(response.Content ?? "");
+                var content = res?.result;
+                if (content?.code != ResponseCodes.ResponseCodeCreated)
+                    return res;
+                return res;
+            }
+            catch (Exception ex)
+            {
+                return new ResponseObject()
+                {
+                    result = new ResponseContents()
+                    {
+                        message = "Error! Something went wrong trying to create event type, please try again later.",
+                    }
+                };
+            }
+        }
+
+
+
+        public async Task<ResponseObject> UpdateEventTypes(string id, CreateEentTypesDto model)
+        {
+            try
+            {
+                var headers = await _webService.GetAuthorizationHeaders();
+                var response = await _webService.Call(ApiUrl, $"update-event-type/{id}", Method.Put, model, headers);
+                var res = JsonConvert.DeserializeObject<ResponseObject>(response.Content ?? "");
+                var content = res?.result;
+                if (content?.code != ResponseCodes.ResponseCodeOk)
+                    return res;
+                return res;
+            }
+            catch (Exception ex)
+            {
+                return new ResponseObject()
+                {
+                    result = new ResponseContents()
+                    {
+                        message = "Error! Something went wrong trying to update event type, please try again later.",
+                    }
+                };
+            }
+        }
+
+
+
+        public async Task<ResponseObject> SwitchEventTypes(string id, SwitchEventTypeDto model)
+        {
+            try
+            {
+                var headers = await _webService.GetAuthorizationHeaders();
+                var response = await _webService.Call(ApiUrl, $"switch-event-type/{id}", Method.Put, model, headers);
+                var res = JsonConvert.DeserializeObject<ResponseObject>(response.Content ?? "");
+                var content = res?.result;
+                if (content?.code != ResponseCodes.ResponseCodeOk)
+                    return res;
+                return res;
+            }
+            catch (Exception ex)
+            {
+                return new ResponseObject()
+                {
+                    result = new ResponseContents()
+                    {
+                        message = "Error! Something went wrong trying to switch event type, please try again later.",
+                    }
+                };
+            }
+        }
+
+        public async Task<ResponseObject> RemoveEventTypes(string id)
+        {
+            try
+            {
+                var headers = await _webService.GetAuthorizationHeaders();
+                var response = await _webService.Call(ApiUrl, $"remove-event-type/{id}", Method.Delete, null, headers);
+                var res = JsonConvert.DeserializeObject<ResponseObject>(response.Content ?? "");
+                var content = res?.result;
+                if (content?.code != ResponseCodes.ResponseCodeOk)
+                    return res;
+                return res;
+            }
+            catch (Exception ex)
+            {
+                return new ResponseObject()
+                {
+                    result = new ResponseContents()
+                    {
+                        message = "Error! Something went wrong trying to switch event type, please try again later.",
                     }
                 };
             }
--- ivs.Domain/Models/Dtos/Organisations/CreateOrganizationDto.cs
+++ ivs.Domain/Models/Dtos/Organisations/CreateOrganizationDto.cs
@@ -6,9 +6,9 @@ namespace ivs.Domain.Models.Dtos.Organisations
     {
         [Required(ErrorMessage = "Organisation name is required")]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "Organisation name must be at least 3 characters long")]
-        public string name { get; set; }
+        public string? name { get; set; }
 
         [Required(ErrorMessage = "Organisation description is required")]
-        public string description { get; set; }
+        public string? description { get; set; }
     }
 }
