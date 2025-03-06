949fe3731641efa95519780f8c08b11cdeecc21a
Victor Nwogu <vicmee76@hotmail.com>
2025-03-06 16:03:20 +0100

    -- added side panel for viewing events

--- ivs.Domain/Models/ViewModels/Events/CreateEventVM.cs
+++ ivs.Domain/Models/ViewModels/Events/CreateEventVM.cs
@@ -10,6 +10,10 @@ public class CreateEventVM
     [MinLength(3, ErrorMessage = "Event name should be more than 3 characters.")]
     public string? eventName { get; set; }
     
+    [MinLength(2, ErrorMessage = "Should be more than 2 characters.")]
+    [RegularExpression(@"^[a-zA-Z0-9-]+$", ErrorMessage = "Only letters, numbers, and '-' are allowed.")]
+    public string? eventCode { get; set; }
+    
     [Required(ErrorMessage = "Event description is required")]
     public string? eventDescription { get; set; }
     
